03300bed762570aab5ec08e5c90aa3310c343776
Matt Ravenmoore <m.ravenmoore@gmail.com>
2020-12-08 23:02:50 -0800

    lab 2 finished and ready for test and deploy

---
+++ __tests__/server.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { server } = require('../lib/server');
+const supertest = require('supertest');
+const mockRequest = supertest(server);
+
+describe('web server', () => {
+  it('should respond with a 404', () => {
+    return mockRequest
+      .get('/purplehaze')
+      .then(results => {
+        expect(results.status).toBe(404);
+      }).catch(console.error);
+  });
+});
+describe('web server', () => {
+  it('should respond with a 404', () => {
+    return mockRequest
+      .get('/purplehaze')
+      .then(results => {
+        expect(results.status).toBe(404);
+      }).catch(console.error);
+  });
+});
+
+describe('web server', ()=> {
+  it('should respond with a 500', ()=>{
+    return mockRequest
+      .get('/person')
+      .then(results =>{
+        expect(results.status).toBe(500);
+      }).catch(console.error);
+  });
+});
+describe('web server', ()=> {
+  it('should respond with a 200', ()=>{
+    return mockRequest
+      .get('/person?name=jo')
+      .then(results =>{
+        expect(results.status).toBe(200);
+      }).catch(console.error);
+  });
+});
+
